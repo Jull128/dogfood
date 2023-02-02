cbc4a8fdb350187611537c1e8d0b1a8003d4bac9
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-02-02 21:10:43 +0500

    user work

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react"
 import { api } from "../../api/api";
-
-import { InfoUser } from '../../components/InfoUser/InfoUser'
+import style from './style.module.css'
 
 
 export function User() {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       const token = localStorage.getItem('token');
-      // const res = await api.allUsers(token);
+
       const res = await api.me(token);
       const responce = await res.json()
 
@@ -24,8 +23,34 @@ export function User() {
   }, [])
 
   return (
-    <div>
-      <InfoUser user={user} />
+    <div className={style.container}>
+      <h2>Личный кабинет</h2>
+      <div className={style.block}>
+        <div className={style.avatar}><img alt="" /></div>
+        <div>
+          <p>
+            Фамилия Имя :
+            {' '}
+            <span>{user.name}</span>
+          </p>
+          <p>
+            Тип :
+            {' '}
+            <span>{user.about}</span>
+          </p>
+          <p>
+            группа :
+            {' '}
+            <span>{user.group}</span>
+          </p>
+          <p>
+            email :
+            {' '}
+            <span>{user.email}</span>
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
